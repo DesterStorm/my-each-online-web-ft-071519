4d8e1086bae61bda0cf589253dcf36dd0a66786a
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-21 02:32:27 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,3 +4,12 @@ def my_each(collection)
     puts i
   end
 end
+
+def my_each(array)
+   collection = 0
+   while collection < array.length
+      yield array[collection]
+      collection+=1
+   end
+   array
+end
